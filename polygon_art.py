41ade12899fa31ac895aa183ec89299949504a48
Patthiaon Panitanont <patthiaon.p@ku.th>
2024-11-07 16:45:14 +0700

    polygon_art

--- polygon_art.py
+++ polygon_art.py
@@ -1,52 +1,113 @@
 import turtle
 import random
-
-def draw_polygon(num_sides, size, orientation, location, color, border_size):
-    turtle.penup()
-    turtle.goto(location[0], location[1])
-    turtle.setheading(orientation)
-    turtle.color(color)
-    turtle.pensize(border_size)
-    turtle.pendown()
-    for _ in range(num_sides):
-        turtle.forward(size)
-        turtle.left(360/num_sides)
-    turtle.penup()
-
-def get_new_color():
-    return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-
+choice = int(input('Which art do you want to generate?\nEnter a number between 1 to 9 inclusive: '))
 turtle.speed(0)
 turtle.bgcolor('black')
 turtle.tracer(0)
 turtle.colormode(255)
+#push
 
-# draw a polygon at a random location, orientation, color, and border line thickness
-num_sides = random.randint(3, 5) # triangle, square, or pentagon
-size = random.randint(50, 150)
-orientation = random.randint(0, 90)
-location = [random.randint(-300, 300), random.randint(-200, 200)]
-color = get_new_color()
-border_size = random.randint(1, 10)
-draw_polygon(num_sides, size, orientation, location, color, border_size)
+class Turtle:
+    def __init__(self, choice):
+        self.choice = choice
+        self.num_side = self._num_side()
+        self.size = random.randint(50, 150)
+        self.orientation = random.randint(0, 90)
+        self.location = [random.randint(-300, 300), random.randint(-200, 200)]
+        self.border_size = random.randint(1, 10)
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        self.reduction_ratio = 0.618
 
-# specify a reduction ratio to draw a smaller polygon inside the one above
-reduction_ratio = 0.618
+    def _num_side(self):
+        if self.choice == 1:
+            num_side = choice + 2
+            self.choice = num_side
+            return self.choice
 
-# reposition the turtle and get a new location
-turtle.penup()
-turtle.forward(size*(1-reduction_ratio)/2)
-turtle.left(90)
-turtle.forward(size*(1-reduction_ratio)/2)
-turtle.right(90)
-location[0] = turtle.pos()[0]
-location[1] = turtle.pos()[1]
+        elif self.choice == 2:
+            num_side = choice + 2
+            self.choice = num_side
+            return self.choice
 
-# adjust the size according to the reduction ratio
-size *= reduction_ratio
+        elif self.choice == 3:
+            num_side = choice + 2
+            self.choice = num_side
+            return self.choice
 
-# draw the second polygon embedded inside the original 
-draw_polygon(num_sides, size, orientation, location, color, border_size)
+        elif self.choice == 4:
+            num_side = random.randint(3, 5)
+            self.choice = num_side
+            return self.choice
 
-# hold the window; close it by clicking the window close 'x' mark
+        elif self.choice == 8:
+            num_side = random.randint(3, 5)
+            self.choice = num_side
+            return self.choice
+
+        elif self.choice == 9:
+            num_side = random.randint(3, 5)
+            self.choice = num_side
+            return self.choice
+
+        else:
+            num_side = choice - 2
+            self.choice = num_side
+            return self.choice
+
+    def draw_polygon(self):
+        turtle.penup()
+        turtle.goto(self.location[0], self.location[1])
+        turtle.setheading(self.orientation)
+        turtle.color(self.color)
+        turtle.pensize(self.border_size)
+        turtle.pendown()
+        for _ in range(self.num_side):
+            turtle.forward(self.size)
+            turtle.left(360 / self.num_side)
+        turtle.penup()
+
+    def choice_five(self):
+        turtle.forward(self.size * (1 - self.reduction_ratio) / 2)
+        turtle.left(90)
+        turtle.forward(self.size * (1 - self.reduction_ratio) / 2)
+        turtle.right(90)
+        self.location[0] = turtle.pos()[0]
+        self.location[1] = turtle.pos()[1]
+        self.size *= self.reduction_ratio
+        turtle.penup()
+
+    def choice_n(self):
+        turtle.forward(self.size * (1 - self.reduction_ratio) / 2)
+        turtle.left(90)
+        turtle.forward(self.size * (1 - self.reduction_ratio) / 2)
+        turtle.right(90)
+        self.location[0] = turtle.pos()[0]
+        self.location[1] = turtle.pos()[1]
+        self.size *= self.reduction_ratio
+        turtle.penup()
+
+    def draw(self):
+        for i in range(3):
+            self.draw_polygon()
+            if choice == 9:
+                self.choice_n()
+            if 5 <= choice <= 8:
+                self.choice_five()
+
+
+# for _ in range(20):
+#     drawing = Turtle(choice)
+#     drawing.draw()
+if 1 <= choice <= 8:
+    for _ in range(20):
+        drawing = Turtle(choice)
+        drawing.draw()
+if choice == 9:
+    for i in range(8):
+        for u in range(2):
+            drawing = Turtle(choice)
+            drawing.draw()
+    drawing = Turtle(choice)
+    drawing.draw_polygon()
+
 turtle.done()
